4ca8c84f70beab6785b37862f606bcd6f0e19877
Josue Rivera <prbassplayer@gmail.com>
2014-07-31 20:48:42 +0200

    Update script
    Change-Id: I3cb64cfd3b25c7073f2d586d6333a1f0d34ec50f
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- make.sh
+++ make.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Stupid shell script to compile kernel, nothing fancy
+ID="`pwd`"
 
 # Exports all the needed things Arch, SubArch and Cross Compile
 export ARCH=arm
 echo 'exporting Arch'
 export SUBARCH=arm
 echo 'exporting SubArch'
-export CROSS_COMPILE=/home/prbassplayer/aosp/prebuilt/linux-x86/toolchain/linaro-4.8-13.06/bin/arm-linux-gnueabihf-
-#export CROSS_COMPILE=/home/prbassplayer/WIP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-echo 'exporting Cross Compile'
+if echo "$ID" | grep josh ; then
+export CROSS_COMPILE=/home/josh/Android/android_prebuilt/linux-x86/toolchain/linaro-4.8-13.06/bin/arm-linux-gnueabihf-
+echo 'exporting Cross Compile for HOME'
+else
+export CROSS_COMPILE=/home/prbassplayer/caf/prebuilt/linux-x86/toolchain/linaro-4.8-13.06/bin/arm-linux-gnueabihf-
+echo 'exporting Cross Compile server'
+fi
 
 # Make sure build is clean!
 echo 'Cleaning build'
